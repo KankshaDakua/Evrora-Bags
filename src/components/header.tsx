8bae220890e7ad8f642ea1cff5b541028f27fbf9
Kanksha Dakua <kanksha10.KD@gmail.com>
2025-08-19 13:35:04 +0000

    whats wrong with you i have opened this website on my mobile i am clicki

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -47,7 +47,7 @@ const Header = () => {
         <div className="md:hidden">
           <Sheet open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
             <SheetTrigger asChild>
-              <Button variant="ghost" size="icon">
+              <Button variant="ghost" size="icon" onClick={() => setIsMobileMenuOpen(true)}>
                 <Menu className="h-6 w-6 text-primary" />
                 <span className="sr-only">Open menu</span>
               </Button>
